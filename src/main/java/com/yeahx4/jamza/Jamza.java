7284216273590727b306fad4c165c3d33098f771
hellun205 <hellun515@gmail.com>
2022-07-23 18:25:57 +0900

    build encoding

--- src/main/java/com/yeahx4/jamza/Jamza.java
+++ src/main/java/com/yeahx4/jamza/Jamza.java
@@ -7,7 +7,7 @@ public class Jamza {
     public static void main(String[] args) {
         Console.println(ConsoleColor.ANSI_BLACK, ConsoleColor.ANSI_WHITE_BACKGROUND, "Welcome to Jamza");
 
-//        Console.println(ConsoleColor.ANSI_WHITE, ConsoleColor.ANSI_BLACK_BACKGROUND, String.format("input string: %s", Console.read()));
-//        Console.println(ConsoleColor.ANSI_WHITE, ConsoleColor.ANSI_BLACK_BACKGROUND, String.format("input int: %d", Console.read_int()));
+        Console.println(ConsoleColor.ANSI_WHITE, ConsoleColor.ANSI_BLACK_BACKGROUND, String.format("%s를 입력했므", Console.read()));
+        Console.println(ConsoleColor.ANSI_WHITE, ConsoleColor.ANSI_BLACK_BACKGROUND, String.format("input int: %d", Console.read_int()));
     }
 }
