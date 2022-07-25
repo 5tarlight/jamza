e6adb45f7e523550cd0333897894077963f3eee2
YEAHx4 <developerstarlight@gmail.com>
2022-07-25 23:48:41 +0900

    Write new champion file

--- src/main/java/com/yeahx4/jamza/champion/Player.java
+++ src/main/java/com/yeahx4/jamza/champion/Player.java
@@ -2,9 +2,19 @@ package com.yeahx4.jamza.champion;
 
 import com.yeahx4.jamza.util.Console;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * Champion util static class
@@ -17,6 +27,7 @@ public final class Player {
      * Directory where champion data stored
      */
     public static final String dir = "./data/player";
+    private static File df = new File(dir);
 
     /**
      * Create new instance of champion with nickname
@@ -56,6 +67,7 @@ public final class Player {
         if (input == null)
             return false;
 
+        List<String> names = getChampionFileList();
         String nickname = Console.readLine(
                 "이름을 지정해주세요 : ",
                 str -> {
@@ -63,9 +75,10 @@ public final class Player {
                     Matcher matcher = pattern.matcher(str);
                     boolean reg = matcher.matches();
 
-                    Console.println(Boolean.toString(reg));
-
-                    return !reg && str.length() > 0 && str.length() < 15;
+                    return reg &&
+                            str.length() > 0 &&
+                            str.length() < 15 &&
+                            !names.contains(str);
                 }
         );
 
@@ -80,10 +93,28 @@ public final class Player {
      * When rename champion, migrate old file too.
      *
      * @param champion champion class to write
-     * @return
+     * @return result
      */
     public static boolean saveChampion(Champion champion) {
-        return false;
+        File df = new File(dir);
+        File file = new File(df, String.format("%s.champion", champion.nickname));
+
+        try {
+            if (!df.exists()) df.mkdirs();
+            if (file.exists()) file.delete();
+            file.createNewFile();
+
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(champion);
+
+            oos.close();
+            fos.close();
+
+            return true;
+        } catch (IOException io) {
+            return false;
+        }
     }
 
     /**
@@ -93,5 +124,33 @@ public final class Player {
      */
     public static boolean loadChampionFromFile() {
         return false;
+    }
+
+    /**
+     * Check base directory is exists and create if not.
+     * This method need to be called in the head of methods using file system.
+     */
+    private static void checkDir() {
+        if (!df.exists()) df.mkdirs();
+    }
+
+    /**
+     * Get all champions' name stored in file.
+     *
+     * @return List of names
+     */
+    public static List<String> getChampionFileList() {
+        try {
+            checkDir();
+            List<String> files = Files.walk(Paths.get(dir))
+                    .filter(Files::isRegularFile)
+                    .map(Path::toFile)
+                    .map(f -> f.getName())
+                    .toList();
+
+            return files;
+        } catch (IOException ex) {
+            return null;
+        }
     }
 }
